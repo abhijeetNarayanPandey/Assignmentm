e89c442f2532e571b144005d9de2394dbffae509
DESKTOP-MIJ082F\Admin <pabhijeet474@gmail.com>
2025-05-11 01:26:55 +0530

    second commit

--- app/src/main/java/com/example/assignmentm/di/AppModule.kt
+++ app/src/main/java/com/example/assignmentm/di/AppModule.kt
@@ -2,12 +2,14 @@
 package com.example.assignmentm.di
 
 import com.example.assignmentm.data.remote.api.ProductApi
+import com.example.assignmentm.data.repository.ProductRepositoryImpl
+import com.example.assignmentm.domain.repository.ProductRepository
+import com.example.assignmentm.domain.use_case.GetProductByIdUseCase
+import com.example.assignmentm.domain.use_case.GetProductsUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -18,28 +20,34 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
-        val loggingInterceptor = HttpLoggingInterceptor().apply {
-            setLevel(HttpLoggingInterceptor.Level.BODY)
-        }
-        return OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .build()
-    }
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://api.themoviedb.org/3/")
-            .client(okHttpClient)
+            .baseUrl("https://dummyjson.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
     @Provides
     @Singleton
-    fun provideApiService(retrofit: Retrofit): ProductApi {
+    fun provideProductApi(retrofit: Retrofit): ProductApi {
         return retrofit.create(ProductApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideProductRepository(api: ProductApi): ProductRepository {
+        return ProductRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetProductsUseCase(repository: ProductRepository): GetProductsUseCase {
+        return GetProductsUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetProductByIdUseCase(repository: ProductRepository): GetProductByIdUseCase {
+        return GetProductByIdUseCase(repository)
     }
 }
